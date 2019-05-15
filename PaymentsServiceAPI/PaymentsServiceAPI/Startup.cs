f8fab879b4570b7c3ff321bbff8e5b307251aa79
Mkhululi Mtshemla <201402344@student.uj.ac.za>
2019-05-16 00:23:02 +0200

    add startup

--- PaymentsServiceAPI/PaymentsServiceAPI/Startup.cs
+++ PaymentsServiceAPI/PaymentsServiceAPI/Startup.cs
@@ -25,7 +25,77 @@ namespace PaymentsServiceAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            ConfigureInjectedOrchestrations(services);
+            ConfigureInjectedServices(services);
+            ConfigureInjectedMappings(services);
+
+            services.AddCors(options1 =>
+            {
+                options1.AddPolicy("MyCorsPolicy", builder => builder
+                    .AllowAnyMethod()
+                    .AllowCredentials()
+                    .AllowAnyOrigin()
+                    .WithHeaders("Accept", "Content-Type", "Origin", "X-My-Header"));
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Payments Service API",
+                    Description = "A simple payments Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Mkhululi Mtshemla",
+                        Email = "201402344@student.uj.ac.za",
+                        //Url = "https://twitter.com/spboyer"
+                    }
+                });
+            });
+        }
+
+        public void ConfigureInjectedServices(IServiceCollection services)
+        {
+        }
+
+        public void ConfigureInjectedMappings(IServiceCollection services)
+        {
+            services.AddSingleton<INotificationMapping, NotificationMapping>();
+        }
+
+        public void ConfigureInjectedOrchestrations(IServiceCollection services)
+        {
+            services.AddSingleton<INotificationOrchestration, NotificationOrchestration>();
+            services.AddSingleton<IMarketOrchestration, MarketOrchestration>();
+            services.AddSingleton<IUserOrchestration, UserOrchestration>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,13 +105,18 @@ namespace PaymentsServiceAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
+            //TODO uncomment when we go live and add our teams computers IPs the the white list
+            //app.UseMiddleware<IpRestrictionMiddleware>();
+
+            app.UseSwagger();
+            app.UseCors("MyCorsPolicy");
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("v1/swagger.json", "Payment Service API v1");
+            });
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
